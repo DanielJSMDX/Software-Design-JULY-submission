5c01dbb14fc9c670d20aab7c77c7cab0624ac2a4
DanielJSMDX <ds1362@live.mdx.ac.uk>
2023-07-11 14:47:50 +0100

    This is the skeleton of the code. this version has a list with sublists that scales how many there are in relation to the list of your starting list.

--- out/production/Software-Design-JULY-submission/Software Design/src/Main.java
+++ out/production/Software-Design-JULY-submission/Software Design/src/Main.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
-        ArrayList<String> m_list = new ArrayList<>(Arrays. asList("a", "d", "b", "c", "e", "f", "h", "g"));
+        ArrayList<String> m_list = new ArrayList<>(Arrays. asList("a", "d", "b", "c", "e", "f", "h", "g")); //starting list
         splitter(m_list);
 
     }
     private static void splitter(ArrayList<String> x) {
         System.out.println("Starting List: " + x);
-        int list_count = x.size()/2;
+        int list_count = x.size()/2; //to find the amount of lists required for later
         System.out.println(list_count);
 
-        List<List<String>> all_parts = new ArrayList<>();
-        for (int L = 0; L<list_count; L++) {
+        List<List<String>> all_parts = new ArrayList<>(); //A list that can hold other lists.
+        for (int L = 0; L<list_count; L++) { // creates the different parts for all_parts, to fill in later
             List<String> part = new ArrayList<>();
             all_parts.add(part);
         }
