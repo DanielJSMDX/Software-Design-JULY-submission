adbcd7642f32fe6d21420cbe40eebe4ef55eaa57
DanielJSMDX <ds1362@live.mdx.ac.uk>
2023-07-11 15:21:36 +0100

    this is the 2nd version. This version has a functioning for loop that allows for the sublists to have the required elements for later use provided by the starting list without breaking the order of said list.

--- out/production/Software-Design-JULY-submission/Software Design/src/Main.java
+++ out/production/Software-Design-JULY-submission/Software Design/src/Main.java
@@ -12,14 +12,21 @@ public class Main {
     private static void splitter(ArrayList<String> x) {
         System.out.println("Starting List: " + x);
         int list_count = x.size()/2; //to find the amount of lists required for later
-        System.out.println(list_count);
 
         List<List<String>> all_parts = new ArrayList<>(); //A list that can hold other lists.
         for (int L = 0; L<list_count; L++) { // creates the different parts for all_parts, to fill in later
             List<String> part = new ArrayList<>();
             all_parts.add(part);
         }
-        System.out.println(all_parts);
+        int pos = 0;
+        for (String s : x) {// adds all the elements from the starting set into the corresponding sublist
+            if (all_parts.get(pos).size() >= 2) {//moves on to next sublist when two elements are in the currently viewed sublist.
+                pos = pos + 1;
+            }
+            all_parts.get(pos).add(s); // adds currently viewed element from the main list into the correct part sublist.
+        }
+        System.out.println("current arrangement: " + all_parts);
+
 
     }
 
