b7ce493a07fc3fe56b948081d95c2639f64fc62e
DanielJSMDX <ds1362@live.mdx.ac.uk>
2023-07-12 01:00:19 +0100

    This is the 7th version:
    - works with starting lists that have an odd number of elements.
    - should be the finalised version.

--- out/production/Software-Design-JULY-submission/Software Design/src/Main.java
+++ out/production/Software-Design-JULY-submission/Software Design/src/Main.java
@@ -3,22 +3,18 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 
-import java.util.*;
-import java.util.Arrays;
-import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("running MAIN");
-        ArrayList<String> m_list = new ArrayList<>(Arrays.asList("d", "a", "b", "c", "e", "f", "h", "g")); //starting list
+        ArrayList<String> m_list = new ArrayList<>(Arrays.asList("Dopey", "Doc",
+                "Sleepy", "Happy", "Bashful", "Sneezy", "Grumpy")); //starting list
         splitter(m_list);
 
     }
 
     private static void splitter(ArrayList<String> x) {
-        System.out.println("running SPLITTER");
-        int list_count = x.size() / 2; //to find the amount of lists required for later
+        int list_count = Math.ceilDiv(x.size(), 2); //to find the amount of lists required for later
 
         List<List<String>> all_parts = new ArrayList<>(); //A list that can hold other lists.
         for (int L = 0; L < list_count; L++) { // creates the different parts for all_parts, to fill in later
@@ -38,7 +34,6 @@ public class Main {
     }
 
     private static void organiser(List<List<String>> all_parts, ArrayList<String> comparer) { // goes through each part and makes sure they are organised.
-        System.out.println("running organiser");
 
         List<List<Integer>> organised_count_scores = new ArrayList<>();
         int list_count = all_parts.size(); //to find the amount of lists required for later
@@ -48,33 +43,38 @@ public class Main {
         }
         int organised_count = 0;//to add into the sublists of "organised_count_scores"
 
-        for (int l = 0; l < all_parts.size(); l++) {//the main for loop goes through and clarifies that the values in organised_count_score are correct to the groups.
+        for (int l = 0; l < all_parts.size();l++) {//the main for loop goes through and clarifies that the values in organised_count_score are correct to the groups.
+            if(all_parts.get(l).size() == 1) {
+                organised_count_scores.set(l, Collections.singletonList(organised_count));
 
-            for (int p = 0; p < all_parts.get(l).size() - 1;) {
-                if (all_parts.get(l).get(p).compareToIgnoreCase(all_parts.get(l).get(p + 1)) < 0) {
-                    organised_count = organised_count + 1;
-                    organised_count_scores.set(l, Collections.singletonList(organised_count));
-                    p=p+1;
+            }else{
+                for (int p = 0; p < all_parts.get(l).size() - 1;) {
+                    if (all_parts.get(l).get(p).compareToIgnoreCase(all_parts.get(l).get(p + 1)) < 0 ) {
+                        organised_count = organised_count + 1;
+                        organised_count_scores.set(l, Collections.singletonList(organised_count));
+                        p=p+1;
+
+
+                    } else if (all_parts.get(l).get(p).compareToIgnoreCase(all_parts.get(l).get(p + 1)) > 0) {
+                        Collections.swap(all_parts.get(l), p, p + 1);
+                        p=0;
+                        l=0;
+                        organised_count = 0;
+
+
+
+                    }
 
                 }
-                else if (all_parts.get(l).get(p).compareToIgnoreCase(all_parts.get(l).get(p + 1)) > 0) {
-                    String temp1 = all_parts.get(l).get(p);
-                    Collections.swap(all_parts.get(l), p, p + 1);
-                    p=0;
-                    organised_count = 0;
-
-
-
-                }
-
             }
+
             organised_count = 0;// resets value for the next run through of the main for loop
 
         }
 
         int verified_score = 0;
         while(verified_score <all_parts.size()) {// this is a validation process to compare the overall scores gathered above to how many verifications should have taken place
-                                                 // I'm not entirely sure why, but this IS needed even though it does a similar process to the above segment.
+                                                 // I'm not entirely sure why, but this IS needed even though it does a similar process to the above segment
             for (int o=0;o<organised_count_scores.size();) {
 
 
@@ -92,7 +92,8 @@ public class Main {
         if (verified_score == all_parts.size() && all_parts.size()>1) {//sends it to the combiner if there's still two groups/sublists to compare
             combiner(all_parts,comparer);
         } else if(verified_score == all_parts.size() && all_parts.size() == 1) {// prints the remaining sublist with all the content (the format of the "all_parts" has the list sitll contained inside another list.)
-            System.out.println(all_parts.get(0));
+            System.out.println("Starting set was: " + comparer);
+            System.out.println("Final organised sed is: " + all_parts.get(0));
         }else{
             organiser(all_parts, comparer); //just incase something changed at the end that may have been missed.
         }
@@ -101,7 +102,6 @@ public class Main {
     }
 
     private static void combiner(List<List<String>> all_parts, ArrayList<String> comparer_for_organiser) {// this will start combining the different parts together to start the official merge sort.
-        System.out.println("running COMBINER");
         List<List<String>> merged_parts = new ArrayList<>();
         int list_count = all_parts.size() / 2; //to find the amount of lists required for later
         for (int L = 0; L < list_count; L++) { // creates the different parts for all_parts, to fill in later
